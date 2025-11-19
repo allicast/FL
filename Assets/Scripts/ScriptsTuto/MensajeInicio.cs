594bba4bf8d76083509bd6b41674c112056449d2
TheLara270 <castillorodriguezallisonnicole@gmail.com>
2025-11-19 15:58:15 -0500

    paneles de tuto

--- Assets/Scripts/ScriptsTuto/MensajeInicio.cs
+++ Assets/Scripts/ScriptsTuto/MensajeInicio.cs
@@ -5,12 +5,21 @@ using TMPro;
 
 public class MensajeInicio : MonoBehaviour
 {
+    public GameObject texto1;
+    public GameObject texto2;
     public TMP_Text mensaje;
     public float tiempoVisible;
+
+    public float delayTexto1; // tiempo antes de aparecer
+    public float duracionTexto1; // tiempo visible
+
+    public float delayTexto2;
+    public float duracionTexto2;
 
     void Start()
     {
         StartCoroutine(OcultarMensaje());
+        StartCoroutine(ControlarTextos());
     }
 
     IEnumerator OcultarMensaje()
@@ -18,5 +27,20 @@ public class MensajeInicio : MonoBehaviour
         mensaje.gameObject.SetActive(true);
         yield return new WaitForSeconds(tiempoVisible);
         mensaje.gameObject.SetActive(false);
+    }
+
+    IEnumerator ControlarTextos()
+    {
+        // TEXTO 1
+        yield return new WaitForSeconds(delayTexto1);
+        texto1.SetActive(true);
+        yield return new WaitForSeconds(duracionTexto1);
+        texto1.SetActive(false);
+
+        // TEXTO 2
+        yield return new WaitForSeconds(delayTexto2 - (delayTexto1 + duracionTexto1));
+        texto2.SetActive(true);
+        yield return new WaitForSeconds(duracionTexto2);
+        texto2.SetActive(false);
     }
 }
