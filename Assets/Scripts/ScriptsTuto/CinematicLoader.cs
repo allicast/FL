47cbd76cb9150c60833b30e8f178755cff7999e1
allicast <castillorodriguezallisonnicole@gmail.com>
2025-12-08 21:31:22 -0500

    cinematicainicio

---
+++ Assets/Scripts/ScriptsTuto/CinematicLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CinematicaInicio : MonoBehaviour
+{
+    public float cinematicDuration; // tiempo total
+
+    void Start()
+    {
+        Debug.Log("Cinemática iniciada");
+        StartCoroutine(CinematicRoutine());
+    }
+
+    IEnumerator CinematicRoutine()
+    {
+        yield return new WaitForSeconds(cinematicDuration);
+
+        Debug.Log("Cargando escena Dialogosinicio...");
+        SceneManager.LoadScene(2);
+    }
+}
+
+
